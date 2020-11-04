46b61cf29a8da935a18c7f3c79fd3fa7e9cb525b
tom_glb <goodMorning_glb@bigjava.com>
2020-11-04 09:37:49 +0800

    初始化

---
+++ src/test/java/org/bigjava/idea_git/IdeaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bigjava.idea_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
