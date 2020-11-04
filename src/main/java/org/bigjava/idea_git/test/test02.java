2744ac50eeba876241f0fc9908d19c50c5c75e12
tom_glb <goodMorning_glb@bigjava.com>
2020-11-04 11:05:37 +0800

    提交test02

---
+++ src/main/java/org/bigjava/idea_git/test/test02.java
@@ -0,0 +1,11 @@
+package org.bigjava.idea_git.test;
+
+/**
+ * @author gongzm
+ * @ClassName org.bigjava.idea_git.test.test02
+ * @Description
+ * @date 2020-11-04 10:45:34
+ * @version 1.0
+ */
+public class test02 {
+}
